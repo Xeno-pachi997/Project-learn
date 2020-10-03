dbbf289de776832a76c2b39d1ba9b1cffe775e05
Xeno-pachi997 <72313382+Xeno-pachi997@users.noreply.github.com>
2020-10-03 19:34:24 +0200

    Add files via upload

---
+++ python-if.py
@@ -0,0 +1,8 @@
+value = "8"
+if value < "8" :
+    print("The value is less than 8")
+elif value == "8" :
+    print("The value is goddamnit 8!!")
+else:
+    print("The value is higher than what we're looking for")
+print("Finished")
